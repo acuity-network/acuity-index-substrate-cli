311416d64f8310927702f1657c495429f84734fa
Jonathan Brown <jbrown@acuity.social>
2024-02-27 08:36:44 +0700

    Connect to indexer.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use tokio_tungstenite::connect_async;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    /// URL of Substrate node to connect to
+    #[arg(short, long)]
+    pub url: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let (ws_stream, _) = connect_async(args.url).await.expect("Failed to connect");
+    println!("WebSocket handshake has been successfully completed");
 }
