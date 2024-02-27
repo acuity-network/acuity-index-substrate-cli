3fd34e3882f5e18328dcc675c8ed18f6e45bbff9
Jonathan Brown <jbrown@acuity.social>
2024-02-27 13:35:49 +0700

    Implement size-on-disk

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use hybrid_api::Index;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-pub struct Args {
+pub struct Cli {
     /// URL of Substrate node to connect to
     #[arg(short, long)]
     pub url: String,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Query how much disk space is used by the index.
+    SizeOnDisk,
 }
 
 #[tokio::main]
 async fn main() {
-    let args = Args::parse();
-    let index = Index::connect(args.url).await;
+    let cli = Cli::parse();
+    let mut index = Index::connect(cli.url).await;
+
+    match &cli.command {
+        Commands::SizeOnDisk => {
+            let size = index.size_on_disk().await;
+            println!("Size on disk: {}", size);
+        }
+    }
 }
