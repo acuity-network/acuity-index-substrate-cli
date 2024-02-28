55b50417bd228ddaa11c0fb22da52d38ed47c367
Jonathan Brown <jbrown@acuity.social>
2024-02-28 13:21:06 +0700

    Implement get-events

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use byte_unit::{Byte, UnitType};
-use clap::{Parser, Subcommand};
-use hybrid_api::Index;
+use clap::{Parser, Subcommand, ValueEnum};
+use hybrid_api::{Bytes32, Index, Key, SubstrateKey};
+use std::str::FromStr;
+use subxt::utils::AccountId32;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -19,6 +21,32 @@ enum Commands {
     Status,
     /// Query how much disk space is used by the index.
     SizeOnDisk,
+    /// Query for events with a key
+    GetEvents {
+        /// Key type to search for
+        #[arg(long)]
+        key_type: KeyType,
+        /// Key to search for
+        #[arg(short, long)]
+        key: String,
+    },
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Hash, ValueEnum)]
+pub enum KeyType {
+    AccountId,
+    AccountIndex,
+    BountyIndex,
+    EraIndex,
+    MessageId,
+    PoolId,
+    PreimageHash,
+    ProposalHash,
+    ProposalIndex,
+    RefIndex,
+    RegistrarIndex,
+    SessionIndex,
+    TipHash,
 }
 
 #[tokio::main]
@@ -38,5 +66,28 @@ async fn main() {
                 Byte::from_u64(size).get_appropriate_unit(UnitType::Binary)
             );
         }
+        Commands::GetEvents { key_type, key } => match key_type {
+            KeyType::AccountId => {
+                let account_id = AccountId32::from_str(key).unwrap();
+                let events = index
+                    .get_events(Key::Substrate(SubstrateKey::AccountId(Bytes32(
+                        account_id.0,
+                    ))))
+                    .await;
+                println!("Events: {:?}", events);
+            }
+            KeyType::AccountIndex => {}
+            KeyType::BountyIndex => {}
+            KeyType::EraIndex => {}
+            KeyType::MessageId => {}
+            KeyType::PoolId => {}
+            KeyType::PreimageHash => {}
+            KeyType::ProposalHash => {}
+            KeyType::ProposalIndex => {}
+            KeyType::RefIndex => {}
+            KeyType::RegistrarIndex => {}
+            KeyType::SessionIndex => {}
+            KeyType::TipHash => {}
+        },
     }
 }
